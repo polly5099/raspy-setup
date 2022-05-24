65e52b30d4ffd5cd8cda25232a4d7be321fda173
Sascha Poell <sascha.poell@gmail.com>
2022-05-24 18:37:31 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+sudo apt install vim -y
 cd ~
 mkdir setup
 cd setup
 git clone https://github.com/polly5099/htpc-download-box
 git clone https://github.com/polly5099/docker-pi-hole
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker pi
 sudo systemctl enable docker
-sudo systemctl start docker
+
+sudo apt-get install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+#raspi-setup enable ssh
