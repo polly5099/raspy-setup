27233c494cee2dd163bdbb3575bdf65f28720579
polly5099 <sascha.poell@gmail.com>
2022-06-14 19:30:19 +0200

    pupu

--- setup.sh
+++ setup.sh
@@ -14,6 +14,11 @@ sudo systemctl enable docker
 sudo apt-get install libffi-dev libssl-dev
 sudo apt install python3-dev
 sudo apt-get install -y python3 python3-pip
+sudo apt install samba samba-common smbclient
+sudo cp ./smb.conf /etc/samba/smb.conf
+sudo systemctl restart smbd.service
+sudo systemctl restart nmbd.service
 sudo pip3 install docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 #raspi-setup enable ssh
+#mount shit
